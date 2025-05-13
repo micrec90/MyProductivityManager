4d8af0a79d915c282d1a10b11b6722ebee23f310
Michal Reclaw <micrec90@gmail.com>
2025-05-13 22:49:33 +0200

    Adjustments, watermark application and import/export of multiple images, List of images

---
+++ MyProductivityManager/MyProductivityManager/Core/Models/WatermarkEditor/ImageItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MyProductivityManager.Core.Models.WatermarkEditor
+{
+    public class ImageItem : ObservableObject
+    {
+        private ImageSource _source;
+        public ImageSource Source
+        {
+            get { return _source; }
+            set
+            {
+                _source = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Path { get; set; } = string.Empty;
+        public string FileName { get; set; } = string.Empty;
+    }
+}
--- MyProductivityManager/MyProductivityManager/Core/ViewModels/WatermarkEditorViewModel.cs
+++ MyProductivityManager/MyProductivityManager/Core/ViewModels/WatermarkEditorViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -81,8 +82,8 @@ namespace MyProductivityManager.Core.ViewModels
             }
         }
         private string _imagePath;
-        private ImageSource _previewImage;
-        public ImageSource PreviewImage
+        private ImageItem _previewImage;
+        public ImageItem PreviewImage
         {
             get { return _previewImage; }
             set
@@ -91,18 +92,43 @@ namespace MyProductivityManager.Core.ViewModels
                 OnPropertyChanged();
             }
         }
+        private string[] _imagePaths;
+        public ObservableCollection<ImageItem> ImportedImages { get; set; } = new ObservableCollection<ImageItem>();
+        private List<ImageSource> _selectedImages;
+        public List<ImageSource> SelectedImages
+        {
+            get { return _selectedImages; }
+            set
+            {
+                _selectedImages = value;
+                OnPropertyChanged();
+            }
+        }
         public RelayCommand LoadImageCommand { get; set; }
         public RelayCommand ApplyWatermarkCommand { get; set; }
+        public RelayCommand ApplyWatermarkToAllCommand { get; set; }
         public RelayCommand ExportImageCommand { get; set; }
         public RelayCommand LoadWatermarkImageCommand { get; set; }
+        public RelayCommand RemoveImageCommand { get; set; }
+        public RelayCommand ClearAllCommand { get; set; }
         public WatermarkEditorViewModel()
         {
             WatermarkPositionValues = new ObservableCollection<WatermarkPosition>(Enum.GetValues(typeof(WatermarkPosition)).Cast<WatermarkPosition>());
 
-            LoadImageCommand = new RelayCommand(obj => { LoadImage(); }, obj => true);
-            ApplyWatermarkCommand = new RelayCommand(async obj => { await ApplyWatermarkAsync(); }, obj => true);
-            ExportImageCommand = new RelayCommand(obj => { ExportImage(); }, obj => true);
+            LoadImageCommand = new RelayCommand(obj => { LoadImages(); }, obj => true);
+            ApplyWatermarkCommand = new RelayCommand(async obj => { await ApplyWatermarkAsync(obj); }, obj => true);
+            ExportImageCommand = new RelayCommand(obj => { ExportImages(obj); }, obj => true);
             LoadWatermarkImageCommand = new RelayCommand(obj => { LoadWatermarkImage(); }, obj => true);
+            RemoveImageCommand = new RelayCommand(obj => { RemoveImage(obj); }, obj => true);
+            ClearAllCommand = new RelayCommand(obj => { ClearAllImages(); }, obj => ImportedImages.Count > 0);
+        }
+        private void RemoveImage(object obj)
+        {
+            ImportedImages.Remove((ImageItem) obj);
+        }
+        private void ClearAllImages()
+        {
+            ImportedImages.Clear();
         }
         private void LoadWatermarkImage()
         {
@@ -118,26 +144,52 @@ namespace MyProductivityManager.Core.ViewModels
                 UseImageWatermark = true;
             }
         }
-        private void LoadImage()
+        private void LoadImages()
         {
             var dialog = new OpenFileDialog
             {
                 Filter = "Image Files|*.png;*.jpg;*.jpeg;*.bmp",
-                Multiselect = false
+                Multiselect = true
             };
 
             if (dialog.ShowDialog() == true)
             {
-                _imagePath = dialog.FileName;
-                PreviewImage = new BitmapImage(new Uri(_imagePath));
+                _imagePaths = dialog.FileNames;
+                //SelectedImages = new List<ImageSource>();
+                for (int i = 0; i < _imagePaths.Length; i++)
+                {
+                    ImageSource imageSource = new BitmapImage(new Uri(_imagePaths[i]));
+                    ImageItem imageItem = new ImageItem()
+                    {
+                        Source = imageSource,
+                        Path = _imagePaths[i],
+                        FileName = System.IO.Path.GetFileName(_imagePaths[i])
+                    };
+                    ImportedImages.Add(imageItem);
+                }
+                if (ImportedImages.Count > 0)
+                    PreviewImage = ImportedImages.Last();
             }
         }
-        private async Task ApplyWatermarkAsync()
+        private async Task ApplyWatermarkAsync(object param)
         {
-            if (string.IsNullOrEmpty(_imagePath))
+            if (PreviewImage == null)
                 return;
-
-            var original = new BitmapImage(new Uri(_imagePath));
+            if (param == null)
+                await ApplyWatermartToImageAsync(PreviewImage);
+            else
+            {
+                foreach (var im in ImportedImages)
+                {
+                    await ApplyWatermartToImageAsync(im);
+                }
+            }
+        }
+        private async Task ApplyWatermartToImageAsync(ImageItem imageItem)
+        {
+            if (string.IsNullOrEmpty(imageItem.Path))
+                return;
+            var original = new BitmapImage(new Uri(imageItem.Path));
 
             int width = original.PixelWidth;
             int height = original.PixelHeight;
@@ -177,10 +229,9 @@ namespace MyProductivityManager.Core.ViewModels
                     context.DrawText(formattedText, position);
                 }
             }
-
             var bmp = new RenderTargetBitmap(width, height, original.DpiX, original.DpiY, PixelFormats.Pbgra32);
             bmp.Render(visual);
-            PreviewImage = bmp;
+            imageItem.Source = bmp;
         }
         private Point GetWatermarkPosition(WatermarkPosition position, int imageWidth, int imageHeight, double textWidth, double textHeight)
         {
@@ -211,7 +262,16 @@ namespace MyProductivityManager.Core.ViewModels
 
             }
         }
-        private void ExportImage()
+        private void ExportImages(object param)
+        {
+            if (PreviewImage == null)
+                return;
+            if (param == null)
+                ExportImage(PreviewImage.Source);
+            else
+                ExportAllImagesToZip();
+        }
+        private void ExportImage(ImageSource imageSource)
         {
             var dialog = new SaveFileDialog
             {
@@ -222,7 +282,7 @@ namespace MyProductivityManager.Core.ViewModels
             if (dialog.ShowDialog() == true)
             {
                 var encoder = new PngBitmapEncoder();
-                encoder.Frames.Add(BitmapFrame.Create((BitmapSource)PreviewImage));
+                encoder.Frames.Add(BitmapFrame.Create((BitmapSource) imageSource));
 
                 using (var stream = new FileStream(dialog.FileName, FileMode.Create))
                 {
@@ -230,6 +290,43 @@ namespace MyProductivityManager.Core.ViewModels
                 }
 
                 MessageBox.Show("Image exported successfully!", "Export", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        private void ExportAllImagesToZip()
+        {
+            var dialog = new SaveFileDialog
+            {
+                Filter = "ZIP Archive|*.zip",
+                FileName = $"Watermarked_{DateTime.Now.ToString("yyyyMMddHHmmss")}.zip"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                using (var zipToCreate = new FileStream(dialog.FileName, FileMode.Create))
+                {
+                    using (var archive = new ZipArchive(zipToCreate, ZipArchiveMode.Create))
+                    {
+                        for (int i = 0; i < ImportedImages.Count; i++)
+                        {
+                            var imageSource = ImportedImages[i].Source;
+
+                            using (var imageStream = new MemoryStream())
+                            {
+                                var encoder = new PngBitmapEncoder();
+                                encoder.Frames.Add(BitmapFrame.Create((BitmapSource) imageSource));
+                                encoder.Save(imageStream);
+                                imageStream.Position = 0;
+
+                                var zipEntry = archive.CreateEntry(ImportedImages[i].FileName);
+
+                                using (var entryStream = zipEntry.Open())
+                                {
+                                    imageStream.CopyTo(entryStream);
+                                }
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("Images exported to ZIP successfully!", "Export", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
     }
